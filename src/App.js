7b6f4082021dd4addc1645e022b009bac3c95696
Sakshi Bhagat <sakshibhagat8390@gmail.com>
2025-08-13 09:39:17 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './output.css';
 import WelcomePage from './components/WelcomePage';
 import SignIn from './components/SignIn';
